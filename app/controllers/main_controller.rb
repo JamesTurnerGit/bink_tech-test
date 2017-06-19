5c319a32a122e86e6a368d501b59a331699df673
James Turner <mail.jt@gmail.com>
2017-06-19 16:09:29 +0100

    params reaching controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,13 @@
 class MainController < ApplicationController
   include MainHelper
   def index
+    @random_noun = random_noun
   end
+
   def search
+    colour =  params[:colour]
+    noun   = params[:noun]
+    @random_noun = random_noun
     render "index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '', to: 'main#main'
+  get '', to: 'main#index'
   post '', to: 'main#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
