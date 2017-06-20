f298a33950d9e4a4e68f3afcffba9cb662d974e7
James Turner <mail.jt@gmail.com>
2017-06-20 19:59:11 +0100

    herokuTweaks

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
-require "dotenv/load" unless Rails.env.production?
 require "rails"
+require "dotenv/load" unless Rails.env.production?
 # Pick the frameworks you want:
 require "active_model/railtie"
 # require "active_job/railtie"
